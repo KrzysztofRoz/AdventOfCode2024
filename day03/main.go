2ffc97118d5538c9115ae856cef1b66fba126977
Krzysztof Rozbicki <krzysz.rozbicki@gmail.com>
2024-12-03 21:29:02 +0100

    Add second part

--- day03/main.go
+++ day03/main.go
@@ -9,10 +9,10 @@ import (
 	"strconv"
 )
 
-type MulObject struct {
-	X   int
-	Y   int
-	Mul int
+type Operation struct {
+	Instruction string
+	X           int
+	Y           int
 }
 
 func main() {
@@ -21,27 +21,29 @@ func main() {
 	ans1 := FirstTaskDay03(filePath)
 	fmt.Println("First task:")
 	fmt.Println(ans1)
-	// fmt.Println("=======================Task2=======================")
-	// ans2 := SecondTaskDay02(filePath)
-	// fmt.Println("Second task:")
-	// fmt.Println(ans2)
+	fmt.Println("=======================Task2=======================")
+	ans2 := SecondTaskDay03(filePath)
+	fmt.Println("Second task:")
+	fmt.Println(ans2)
 
 }
 
 func FirstTaskDay03(path string) int {
-	muls, err := ParseInput(path)
+	operations, err := ParseInput(path)
 	if err != nil {
 		log.Fatal(err)
 		panic(err)
 	}
 	count := 0
-	for _, mul := range muls {
-		count += mul.Mul
+	for _, ops := range operations {
+		if ops.Instruction == "mul" {
+			count += ops.X * ops.Y
+		}
 	}
 	return count
 }
 
-func ParseInput(filePath string) (mulData []MulObject, err error) {
+func ParseInput(filePath string) (operations []Operation, err error) {
 	file, err := os.Open(filePath)
 	if err != nil {
 		log.Fatal(err)
@@ -51,36 +53,64 @@ func ParseInput(filePath string) (mulData []MulObject, err error) {
 	scanner := bufio.NewScanner(file)
 
 	for scanner.Scan() {
-		muls, err := ParseLineToMuls(scanner.Text())
+		muls, err := ParseLineToOperations(scanner.Text())
 		if err != nil {
 			log.Fatal(err)
 			panic(err)
 		}
-		mulData = append(mulData, muls...)
+		operations = append(operations, muls...)
 	}
-	return mulData, nil
+	return operations, nil
 }
 
-func ParseLineToMuls(line string) (mulData []MulObject, err error) {
-	r := regexp.MustCompile(`mul\([0-9]{1,3}\,[0-9]{1,3}\)`)
+func ParseLineToOperations(line string) (operations []Operation, err error) {
+	r := regexp.MustCompile(`(mul\([0-9]{1,3}\,[0-9]{1,3}\))|(do\(\))|(don't\(\))`)
 	matchMul := r.FindAllString(line, -1)
 
 	numberRegex := regexp.MustCompile(`[0-9]{1,3}`)
+	doRegex := regexp.MustCompile(`do\(\)`)
 	for _, match := range matchMul {
 		numbers := numberRegex.FindAllString(match, -1)
-
-		x, err := strconv.Atoi(numbers[0])
-		if err != nil {
-			log.Fatal(err)
-			return mulData, err
+		if len(numbers) > 0 {
+			x, err := strconv.Atoi(numbers[0])
+			if err != nil {
+				log.Fatal(err)
+				return operations, err
+			}
+			y, err := strconv.Atoi(numbers[1])
+			if err != nil {
+				log.Fatal(err)
+				return operations, err
+			}
+			operations = append(operations, Operation{X: x, Y: y, Instruction: "mul"})
+		} else if doRegex.MatchString(match) {
+			operations = append(operations, Operation{Instruction: "do"})
+		} else {
+			operations = append(operations, Operation{Instruction: "dont"})
 		}
-		y, err := strconv.Atoi(numbers[1])
-		if err != nil {
-			log.Fatal(err)
-			return mulData, err
-		}
-		mulData = append(mulData, MulObject{X: x, Y: y, Mul: x * y})
-
 	}
-	return mulData, nil
+	return operations, nil
+}
+
+func SecondTaskDay03(path string) int {
+	operations, err := ParseInput(path)
+	if err != nil {
+		log.Fatal(err)
+		panic(err)
+	}
+	count := 0
+	IsEnabled := true
+	for _, ops := range operations {
+		switch ops.Instruction {
+		case "do":
+			IsEnabled = true
+		case "dont":
+			IsEnabled = false
+		case "mul":
+			if IsEnabled {
+				count += ops.X * ops.Y
+			}
+		}
+	}
+	return count
 }
